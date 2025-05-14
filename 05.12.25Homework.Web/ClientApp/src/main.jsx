18bd90859cc96b422cc0cb41b07249543e823e7e
clittman <chavilittman@gmail.com>
2025-05-14 02:27:51 -0500

    Add project files.

---
+++ 05.12.25Homework.Web/ClientApp/src/main.jsx
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NumbersTable from './NumbersTable'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+   <NumbersTable />
+)
